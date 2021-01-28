599f02f40b8123d8ae03b20763bd4cbe0982550c
Felix Anducho <felix.anducho74@gmail.com>
2021-01-28 17:49:13 -0600

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# interviewExercises
+# Interview Questions
+
+1. Reverse String<
+2. Palindrome
+3. Reverse Integer
+4. Max Char
+5. Fizz Buzz
---
+++ index.js
@@ -0,0 +1,93 @@
+////////////////////
+// Reverse String //
+////////////////////
+
+//Easy way, using .reverse()
+
+function reverseStrOne(str){
+    return str.split("")
+           .reverse()
+           .join("")
+}
+
+console.log("Using reverse :", reverseStrOne("Pleopleo"))
+
+//Manual way using a for loop
+
+function reverseStrTwo(str){
+
+    let reversed = ""
+
+    for(let character of str){
+        reversed = character + reversed
+        //console.log(reversed)
+    }
+
+    return reversed
+}
+
+console.log("Using a for loop :", reverseStrTwo("Felix Anducho"))
+console.log("-------------------------------------------------- ")
+
+/////////////////
+// Palindrome  //
+///////////////// 
+
+function palindrome(str){
+    let reversed = ""
+
+    for(character of str){
+        reversed = character + reversed
+    }
+
+    if(str === reversed){
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log("Palindrome: ", palindrome("acca"))
+console.log("Not a Palindrome: ", palindrome("asdfgh")) 
+
+//Using .every() (It makes more comparations, not viable)
+
+function palindromeWithEvery(str){
+    return str.split("").every((char, i) => {
+        return char == str[str.length - i - 1]
+    })
+}
+
+console.log("Palindrome with .every(): ", palindromeWithEvery("abba"))
+console.log("-------------------------------------------------- ")
+
+///////////////////
+// Int reversal  //
+///////////////////
+
+function reverseInt(n){
+    let isNegative = Math.sign(n)
+    const reversedInt = n.toString().split('').reverse().join('')
+    
+    if(isNegative === -1){
+        return parseInt(reversedInt) * -1
+    }
+
+    return reversedInt
+}
+
+console.log(reverseInt(3892))
+console.log(reverseInt(-233))
+
+// Not using Math.sign()
+
+function reverseIntWithoutSign(n){
+    const reversedInt = n.toString().split("").reverse().join("")
+    if(n < 0){
+        return parseInt(reversedInt) * -1
+    }
+    return parseInt(reversedInt)
+}
+
+console.log("Reverse Int without Math.sign(): ", reverseIntWithoutSign(-3531))
+console.log("Reverse Int without Math.sign(): ", reverseIntWithoutSign(8121))
